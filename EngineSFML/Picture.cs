db74bd88486a2f62e1c516b09e956c6bb1839147
Flummox56 <78684236+Flummox56@users.noreply.github.com>
2023-02-12 19:42:24 +0300

    Revert "release 1.1.0"
    This reverts commit e8c90b60c7cce4d1b5e096fd532adfc3134f6321.

--- EngineSFML/Picture.cs
+++ EngineSFML/Picture.cs
@@ -42,9 +42,6 @@ namespace EngineSFML
             OutlineColor = new Color(Color.Red)
         };
 
-        Text t = new Text($"{wait}", new Font("arial.ttf"), 30);
-
-
         List<Sprite> pistonSprites = new List<Sprite>();
 
         Sprite sprite = new Sprite()
@@ -61,14 +58,13 @@ namespace EngineSFML
         {
             for (int i = 0; i < 4; i++)
             {
-                Vector2i[] PoseVectMas = {new Vector2i(80, 10), new Vector2i(359, 10), new Vector2i(637, 10), new Vector2i(915, 10)};
-                Vector2i[] SizeVectMas = {new Vector2i(200, 400), new Vector2i(200, 400), new Vector2i(200, 400), new Vector2i(200, 400)};
-
-                IntRect ir = new IntRect(PoseVectMas[i], SizeVectMas[i]);
-
-                Texture t = new Texture("231221632.png", ir);
-
-                t.Smooth = true;
+                Texture t = new Texture(
+                    "piston_image.jpg",
+                    new IntRect(
+                        new Vector2i(250 * i, 0),
+                        new Vector2i(250, 442)
+                        )
+                    );
 
                 Sprite sp = new Sprite(t);
 
@@ -116,9 +112,6 @@ namespace EngineSFML
 
             sprite.Position = r.Position;
             rw.Draw(sprite);
-
-            t.Position = new Vector2f(500, 450);
-            rw.Draw(t);
         }
 
         public void updateShapes(Object sender, System.Timers.ElapsedEventArgs args)
@@ -169,19 +162,17 @@ namespace EngineSFML
         }
 
         int spriteNum = 0;
-        static double wait;
 
         public void updateSprite(Object sender, System.Timers.ElapsedEventArgs args)
         {
-            
+
             if (Program.eng.Rpm == 0)
             {
                 Thread.Sleep(1);
             }
             else
             {
-                wait = (1000 / (Program.eng.Rpm * 2 / 60));
-                Thread.Sleep((int)wait);
+                Thread.Sleep((int)(1000 / (Program.eng.Rpm * 2 / 60)));
                 if (spriteNum < 3)
                 {
                     spriteNum++;
